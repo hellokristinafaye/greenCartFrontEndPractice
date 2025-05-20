26f46e32677b70dce18a25de9b657d81d8d42ab6
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 13:35:36 -0700

    added state variables to value object so they're accessible in any other component

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,10 +1,17 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 // start with the export line, then allow it to automatically import createContext
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+    // this is the function we use when we want a link to go somewhere instead of an href
+    const navigate = useNavigate();
+    // state variables
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+
     // this is where we'll put all the state variables we want to make available elsewhere (Not declaring them here. That happens above/outisde this function)
-    const value = {}
+    const value = {navigate, user, setUser, isSeller, setIsSeller}
 
     return <AppContext.Provider value={value}>
         {children}
