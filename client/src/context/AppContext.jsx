8d1d491762a588cb061d34d0cf22306a0daacd0e
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 13:24:54 -0700

    useAppContext function added/exported

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,11 +1,17 @@
-import { createContext } from "react";
+import { createContext, useContext } from "react";
 // start with the export line, then allow it to automatically import createContext
 export const AppContext = createContext();
 
+export const AppContextProvider = ({ children }) => {
+    // this is where we'll put all the state variables we want to make available elsewhere (Not declaring them here. That happens above/outisde this function)
+    const value = {}
 
-
-export const AppContextProvider = ({children}) => {
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
+}
+
+// allows us to use the function useAppContext to access any data in the value object
+export const useAppContext = () => {
+    return useContext(AppContext)
 }
