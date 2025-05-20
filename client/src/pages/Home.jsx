53703cdb52f5344230c69b2b69b51799f4f76fca
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 15:38:45 -0700

    main banner mounted in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import MainBanner from '../components/MainBanner'
 
 const Home = () => {
   return (
-    <div>Home</div>
+      <div className="mt-10">
+          <MainBanner/>
+    </div>
   )
 }
 
