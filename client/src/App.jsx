2fd208ac8b43f94da8694e98d17fd4b367f3396b
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 15:46:27 -0700

    conditional for isSeller

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 
 const App = () => {
+  // to allow different rendering depending on whether it's the seller or not.
+  const isSellerPath = useLocation().pathname.includes("seller");
+
   return (
     <div>
       <Navbar />
-      <div className="px-6 md:px-16 lg:px-24 xl:px-32">
+      <div className= {`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path='/' element={<Home/> } />
         </Routes>
