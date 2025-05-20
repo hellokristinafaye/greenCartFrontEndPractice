db84cdb1d7812232446f68fdfd1400884637fece
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 15:39:46 -0700

    Routes imported to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Routes } from 'react-router-dom'
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <div className="">
+        <Routes>
+          
+        </Routes>
+      </div>
     </div>
   )
 }
