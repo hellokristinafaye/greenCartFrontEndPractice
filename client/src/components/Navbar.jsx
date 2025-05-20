5b08f915cf84840e57942643f9ce77cc7f49dd78
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 14:15:06 -0700

    updatting icons to our own assets

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -21,10 +21,7 @@ const Navbar = () => {
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
                     <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M10.836 10.615 15 14.695" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
-                        <path clip-rule="evenodd" d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
-                    </svg>
+                    <img src={assets.search_icon} alt="search" className="w-4 h-4" />
                 </div>
 
                 <div className="relative cursor-pointer">
