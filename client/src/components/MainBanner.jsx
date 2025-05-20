6685ea62bc4c480bf70d778f9287817b1aea48b5
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 15:49:42 -0700

    banner content added

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import { Link } from 'react-router-dom'
 
 const MainBanner = () => {
   return (
       <div className="relative">
           <img src={assets.main_banner_bg} alt="banner" className="w-full hidden md:block" />
           <img src={assets.main_banner_bg_sm} alt="banner" className="w-full md:hidden" />
+          <div className="">
+              <h1 className="">Freshness You Can Trust, Savings You Will Love! </h1>
+          </div>
+          <div className="">
+              <Link>Shop now <img src={assets.white_arrow_icon} alt="arrow icon" className="" /></Link>
+          </div>
     </div>
   )
 }
