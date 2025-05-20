e31b4849ee8d4c098f6a9f52b321422dbce189b7
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 15:20:57 -0700

    nav to cart added

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -32,7 +32,7 @@ const Navbar = () => {
                     <img src={assets.search_icon} alt="search" className="w-4 h-4" />
                 </div>
 
-                <div className="relative cursor-pointer">
+                <div onClick={()=> navigate('/cart')} className="relative cursor-pointer">
                     <img src={assets.cart_icon} alt="cart" className="w-6 opacity-80" />
                     <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
                 </div>
