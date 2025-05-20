07af11a0e13965060493cc026133740fa50926fd
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 15:28:09 -0700

    new change

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
 
     return (
         <nav className="flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white relative transition-all">
-
-            <NavLink to="/" href="#">
+            {/* setOpen(false) will hide mobile menu */}
+            <NavLink to="/" onClick={()=> setOpen(false)} >
                 <img className="h-9" src={assets.logo} alt="logo" />
             </NavLink>
 
