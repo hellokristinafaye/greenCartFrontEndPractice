2656a6ee6367749cc204506c51dab862f34a76e3
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 18:22:38 -0700

    content started and some styling

--- client/src/components/BestSeller.jsx
+++ client/src/components/BestSeller.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const BestSeller = () => {
   return (
-    <div>BestSeller</div>
+      <div className="mt-16">
+          <p className="text-2xl md:text-3xl font-medium">Best Sellers</p>
+          <div className="">
+              
+          </div>
+    </div>
   )
 }
 
