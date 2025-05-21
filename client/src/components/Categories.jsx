dcf77366735901745776701eb50ec9b14eab5eeb
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 18:08:21 -0700

    UAC imported, so we can use navigate

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { assets, categories } from '../assets/assets'
+import { useAppContext } from '../context/AppContext'
 
 const Categories = () => {
+
+    const { navigate } = useAppContext();
+
   return (
       <div className="mt-16">
           <p className="text-2xl md:text-3xl font-medium">Categories</p>
@@ -9,7 +13,12 @@ const Categories = () => {
               
               {/* below is the category card */}
               {categories.map((category, index) => {
-                  <div key={index} className="group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center">
+                  <div key={index} className="group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center"
+                      style={{ backgroundColor: category.bgColor }}
+                      onclick={()=>{
+                        
+                      }}
+                  >
                     <img src={category.image} alt={category.text} className="group-hover:scale-108 transition max-w-28" />
                     <p className="text-sm font-medium">{category.text}</p>
                 </div>
