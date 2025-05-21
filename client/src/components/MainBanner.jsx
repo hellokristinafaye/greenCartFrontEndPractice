ea0a20b2965eafb8b03c3734072243e99efdb0c3
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 17:46:25 -0700

    heading styled

--- client/src/components/MainBanner.jsx
+++ client/src/components/MainBanner.jsx
@@ -8,7 +8,7 @@ const MainBanner = () => {
           <img src={assets.main_banner_bg} alt="banner" className="w-full hidden md:block" />
           <img src={assets.main_banner_bg_sm} alt="banner" className="w-full md:hidden" />
           <div className="">
-              <h1 className="">Freshness You Can Trust, Savings You Will Love! </h1>
+              <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center md:text-left max-w-72 md:max-w-80 lg:max-w-105 leading-tight lg:leading-15">Freshness You Can Trust, Savings You Will Love! </h1>
           </div>
           <div className="flex items-center mt-6 font-medium">
               <Link to={"/products"} className="group flex items-center gap-2 px-7 md:px-9 py-3 bg-primary hover:bg-primary-dull transition rounded text-white cursor-pointer">
