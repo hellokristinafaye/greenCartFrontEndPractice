cc96132db4e599520ffc606c5dd72320fc4b09ec
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-20 18:00:12 -0700

    container styled

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -5,7 +5,7 @@ const Categories = () => {
   return (
       <div className="mt-16">
           <p className="text-2xl md:text-3xl font-medium">Categories</p>
-          <div className="">
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6 xl:grid-cols-7 mt-6 gap-6">
               <div className="">
                   <img src={assets.box_icon} alt="" className="" />
                   <p className="">Fruit</p>
