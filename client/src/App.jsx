3180431a011b193f5142a47b35c1acebba563538
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-31 14:28:30 -0700

    component conditionally mounted

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,21 @@ import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
 import {Toaster} from 'react-hot-toast';
 import Footer from './components/Footer';
+import { useAppContext } from './context/AppContext';
+import Login from './components/Login';
 
 const App = () => {
   // to allow different rendering depending on whether it's the seller or not.
   const isSellerPath = useLocation().pathname.includes("seller");
 
+  const { showUserLogin } = useAppContext();
+
   return (
     <div>
 
       {isSellerPath ? null : <Navbar />}
+      {showUserLogin ? <Login /> : null }
+
 
       <Toaster />
       
