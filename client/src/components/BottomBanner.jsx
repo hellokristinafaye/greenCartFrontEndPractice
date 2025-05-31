6efebb3b62f74de3c80a0c89eee5dae62737cfd1
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-31 12:57:14 -0700

    h1 styled

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -7,7 +7,11 @@ const BottomBanner = () => {
           <img src={assets.bottom_banner_image} alt="banner" className="w-full hidden md:block" />
           <img src={assets.bottom_banner_image_sm} alt="banner" className="w-full md:hidden" />
 
-          <div className="absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24"></div>
+          <div className="absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24">
+              <div className="">
+                  <h1 className="text-2xl md:text-3xl font-semibold text-primary mb-6">Why Are We the Best?</h1>
+              </div>
+          </div>
     </div>
   )
 }
