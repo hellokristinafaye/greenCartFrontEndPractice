3d1639065ebfd9f6deceac231c870b4956cc3495
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-31 16:27:23 -0700

    container styled

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -14,7 +14,7 @@ const ProductCategory = () => {
   return (
       <div>
           {searchCategory && (
-              <div className="">
+              <div className="flex flex-col items-end w-max">
                   <p className="text-2xl font-medium">{searchCategory.text.toUpperCase()}</p>
                   <div className="w-16 h-0.5 bg-primary rounded-full"></div>
               </div>
