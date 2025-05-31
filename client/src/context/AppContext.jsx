5cf9d5d40758dcfae5e6ff31a93cd222a5f410a2
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-30 18:27:58 -0700

    add to cart exported

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -42,7 +42,7 @@ export const AppContextProvider = ({ children }) => {
     },[])
 
     // this is where we'll put all the state variables we want to make available elsewhere (Not declaring them here. That happens above/outisde this function)
-    const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency}
+    const value = {navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin, products, currency, addToCart }
 
     return <AppContext.Provider value={value}>
         {children}
