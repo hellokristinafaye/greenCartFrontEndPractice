5d637733e655584f6a05aec1266b075f5af1d1b8
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-31 16:11:11 -0700

    search input form function coded

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
                 <NavLink to="/">Contact</NavLink>
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
-                    <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
+                    <input onChange={(e)=> setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
                     <img src={assets.search_icon} alt="search" className="w-4 h-4" />
                 </div>
 
