796040f614d1341ee34b3d7827dc17c20ba3918e
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-31 13:06:16 -0700

    descriptions styled

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -14,7 +14,7 @@ const BottomBanner = () => {
                       <div className="flex items-center gap-4 mt-2" key={index} >
                           <img src={feature.icon} alt={feature.title} className="md:w-11 w-9" />
                           <h3 className="text-lg md:text-xl font-semibold">{feature.title}</h3>
-                          <p className="">{feature.description}</p>
+                          <p className="text-gray-500/70 text-xs md:text-sm">{feature.description}</p>
                       </div>
                   ))}
               </div>
