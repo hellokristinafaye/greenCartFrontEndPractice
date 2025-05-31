52bc0d56099c67ce69ac9c9df25f24a0c4b429e2
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-31 14:01:37 -0700

    .text added to link object call, this allowed the page to render

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -30,7 +30,7 @@ const Footer = () => {
                             <ul className="text-sm space-y-1">
                                 {section.links.map((link, i) => (
                                     <li key={i}>
-                                        <a href="#" className="hover:underline transition">{link}</a>
+                                        <a href="#" className="hover:underline transition">{link.text}</a>
                                     </li>
                                 ))}
                             </ul>
