035a5277f2911a30b6c06222179e7e51f493924c
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-30 17:43:55 -0700

    hardcoding rating

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -15,7 +15,7 @@ const ProductCard = ({product}) => {
                 <p className="text-gray-700 font-medium text-lg truncate w-full">{product.name}</p>
                 <div className="flex items-center gap-0.5">
                     {Array(5).fill('').map((_, i) => (<img key={i} src={i<4? assets.star_icon : assets.star_dull_icon} alt="" className="md:w-3.5 w3" />))}
-                    <p>({product.rating})</p>
+                    <p>(4)</p>
                 </div>
                 <div className="flex items-end justify-between mt-3">
                     <p className="md:text-xl text-base font-medium text-indigo-500">
