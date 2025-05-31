cd63e23a17045a6b655b340dbea08ee5dc7b12f3
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-31 15:22:36 -0700

    useEffect finished

--- client/src/pages/AllProducts.jsx
+++ client/src/pages/AllProducts.jsx
@@ -3,12 +3,18 @@ import { useAppContext } from '../context/AppContext'
 
 const AllProducts = () => {
 
-    const { products, searchQuery, setSearchQuery } = useAppContext();
+    const { products, searchQuery } = useAppContext();
     const [filteredProducts, setFilteredProducts] = useState([]);
 
     useEffect(() => {
-        
-    },[])
+        if (searchQuery.length > 0) {
+            setFilteredProducts(products.filter(
+                product => product.name.toLowerCase().includes(searchQuery.toLowerCase())
+            ))
+        } else {
+            setFilteredProducts(products)
+        }
+    },[products, searchQuery])
 
   return (
       <div className="mt-16 flex flex-col">
