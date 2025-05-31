a69eb462fd1cf50abba13d88fc070b211cf12093
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-30 17:46:07 -0700

    infrastructure to import currency to Product Card added

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { assets } from '../assets/assets';
+import { useAppContext } from '../context/AppContext';
 
 const ProductCard = ({product}) => {
     const [count, setCount] = React.useState(0);
 
-
+    const { currency } = useAppContext();
+    
     return (
         <div className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
             <div className="group cursor-pointer flex items-center justify-center px-2">
