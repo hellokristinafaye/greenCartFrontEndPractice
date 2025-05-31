0e7cbf3c0680711ded4b4c27f6e17e8689d31777
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-31 12:54:48 -0700

    div styling added

--- client/src/components/BottomBanner.jsx
+++ client/src/components/BottomBanner.jsx
@@ -6,6 +6,8 @@ const BottomBanner = () => {
       <div className="relative mt-24">
           <img src={assets.bottom_banner_image} alt="banner" className="w-full hidden md:block" />
           <img src={assets.bottom_banner_image_sm} alt="banner" className="w-full md:hidden" />
+
+          <div className="absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24"></div>
     </div>
   )
 }
