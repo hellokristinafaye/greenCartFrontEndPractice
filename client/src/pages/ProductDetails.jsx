011bdbc1ea8c0716cfb79a64e8dfae3b474ac2e4
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-31 17:51:10 -0700

    styling updated

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -56,7 +56,7 @@ const ProductDetails = () => {
 
                     <div className="flex items-center gap-0.5 mt-1">
                         {Array(5).fill('').map((_, i) => (
-                            <img src={i < 4 ? assets.star_icon : assets.star_dull_icon} alt="" className="" />)   
+                            <img src={i < 4 ? assets.star_icon : assets.star_dull_icon} alt="" className="md:w-4 w-3.5" />)   
                         )}
                         <p className="text-base ml-2">({product.rating})</p>
                     </div>
