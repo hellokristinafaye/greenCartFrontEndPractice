34429e3d0006d3e4ff2cf4884b8ebf4108d47bbd
Kristina Faye <hellokristinafaye@gmail.com>
2025-05-31 18:21:00 -0700

    div container styled

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -92,7 +92,7 @@ const ProductDetails = () => {
                     <div className="w-20 h-0.5 bg-primary rounded-full mt-2"></div>
                 </div>
                 {/* next step is to display the products in cardsg */}
-                <div className="">
+                <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-3 md:gap-6 lg:grid-cols-5 mt-6 w-full">
                     {relatedProducts.filter((product) => product.inStock).map((product, index) => (
                         <ProductCard key={index} product={product} />
                     ))}
