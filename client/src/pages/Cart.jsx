725ca17e3e266585844dacc053502f6701dc574b
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 14:10:18 -0700

    div styled

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -29,7 +29,7 @@ const Cart = () => {
     }, [products, cartItems])
  
     return products.length > 0 && cartItems ? (
-        <div className="flex flex-col md:flex-row py-16 max-w-6xl w-full px-6 mx-auto">
+        <div className="flex flex-col md:flex-row mt-16">
             <div className='flex-1 max-w-4xl'>
                 <h1 className="text-3xl font-medium mb-6">
                     Shopping Cart <span className="text-sm text-indigo-500">3 Items</span>
