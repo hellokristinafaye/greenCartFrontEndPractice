7a48b12601624c38023795f34d75839e274f7dd9
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 15:53:55 -0700

    cart total with tax calculating properly! but the cart item qty doens't update(even tho the price does) must debug

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -126,7 +126,9 @@ const Cart = () => {
                         }</span>
                     </p>    
                     <p className="flex justify-between text-lg font-medium mt-3">
-                        <span>Total Amount:</span><span>$20</span>
+                        <span>Total Amount:</span><span>
+                            {currency}${getCartAmount() + getCartAmount() * 2 / 100
+                        }</span>
                     </p>
                 </div>
 
