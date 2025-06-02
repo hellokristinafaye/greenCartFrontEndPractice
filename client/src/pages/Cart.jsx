6b5cd79a4072d4fba2fa1db5574550ce433d9bdf
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 14:11:54 -0700

    cart amount updated dynamically now

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -32,7 +32,7 @@ const Cart = () => {
         <div className="flex flex-col md:flex-row mt-16">
             <div className='flex-1 max-w-4xl'>
                 <h1 className="text-3xl font-medium mb-6">
-                    Shopping Cart <span className="text-sm text-indigo-500">3 Items</span>
+                    Shopping Cart <span className="text-sm text-indigo-500">{getCartCount()} Items</span>
                 </h1>
 
                 <div className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 text-base font-medium pb-3">
