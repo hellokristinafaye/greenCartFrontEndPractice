fe2f7fe1d2f47453589f2c05b737ae4bddc5110a
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 18:05:42 -0700

    line added

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -3,7 +3,10 @@ import React from 'react'
 const MyOrders = () => {
   return (
       <div>
+          <div className="">
           <p className="">My Orders</p>
+              <div className="w-16 h-0.5 bg-primary rounded-full"></div>
+          </div>
     </div>
   )
 }
