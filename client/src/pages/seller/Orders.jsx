dc103b446c20ec77414260cd301cbb95810cb4ad
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-03 14:34:18 -0700

    styled

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -22,7 +22,7 @@ const Orders = () => {
             <h2 className="text-lg font-medium">Orders List</h2>
             {orders.map((order, index) => (
                 <div key={index} className="flex flex-col md:items-center md:flex-row gap-5 justify-between p-5 max-w-4xl rounded-md border border-gray-300">
-                    <div className="flex gap-5">
+                    <div className="flex gap-5 max-w-80">
                         <img className="w-12 h-12 object-cover" src={assets.box_icon} alt="boxIcon" />
                         <>
                             {order.items.map((item, index) => (
