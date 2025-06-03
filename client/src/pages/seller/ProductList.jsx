8020224bb4154fe7247b2af2fb95f994a77d6e2d
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-03 13:53:49 -0700

    leaving inventory in stock for demo purposes

--- client/src/pages/seller/ProductList.jsx
+++ client/src/pages/seller/ProductList.jsx
@@ -32,7 +32,10 @@ const ProductList = () => {
                                     <td className="px-4 py-3 max-sm:hidden">{currency}{product.offerPrice}</td>
                                     <td className="px-4 py-3">
                                         <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
-                                            <input type="checkbox" className="sr-only peer" defaultChecked={product.inStock} />
+                                            <input type="checkbox" className="sr-only peer"
+                                                // he removes this in the tutorial but I think it's in prep for a backend I"m not building yet. 
+                                                defaultChecked={product.inStock}
+                                            />
                                             <div className="w-12 h-7 bg-slate-300 rounded-full peer peer-checked:bg-blue-600 transition-colors duration-200"></div>
                                             <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
                                         </label>
