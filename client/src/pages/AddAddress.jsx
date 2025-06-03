2f1826f705fa1efb107ca74a512c8a1586a04d99
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 17:20:37 -0700

    image spaced/styled

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -12,7 +12,7 @@ const AddAddress = () => {
           <p className="text-2xl md:text-3xl text-gray-500">Add Shipping <span className="font-semibold text-primary">Address</span></p>
           <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
               <div className=""></div>
-              <img src={assets.add_address_iamge} alt="Add Address" className="" />
+              <img src={assets.add_address_iamge} alt="Add Address" className="md:mr-16 mb-16 md:mt-0" />
           </div>
     </div>
   )
