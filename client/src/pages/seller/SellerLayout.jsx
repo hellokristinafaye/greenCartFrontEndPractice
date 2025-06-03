893a9b298e5c12c03a7ba9106999f160d9bcc2ef
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 21:23:19 -0700

    clarity

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -35,8 +35,9 @@ const SellerLayout = () => {
                   <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
                 {sidebarLinks.map((item) => (
                     <NavLink to={item.path} key={item.name} end={item.path === '/seller'}
-                        className={ `flex items-center py-3 px-4 gap-3 
-                            ${index === 0 ? "border-r-4 md:border-r-[6px] bg-indigo-500/10 border-indigo-500 text-indigo-500"
+                            // isActive refers to which of the sidebar links is clicked
+                        className={({ isActive }) => `flex items-center py-3 px-4 gap-3 
+                            ${isActive? "border-r-4 md:border-r-[6px] bg-indigo-500/10 border-indigo-500 text-indigo-500"
                                 : "hover:bg-gray-100/90 border-white text-gray-700"
                             }`
                         }
