daa75842bb68b26d5c88ab16e1decc9632540182
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 17:17:02 -0700

    container styled

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -9,6 +9,9 @@ const AddAddress = () => {
   return (
       <div className="mt-16 pb-16">
           <p className="text-2xl md:text-3xl text-gray-500">Add Shipping <span className="font-semibold text-primary">Address</span></p>
+          <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
+              
+          </div>
     </div>
   )
 }
