3bdec1745ba9238fceedc449310e5ab0f1c268ac
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-03 15:09:19 -0700

    user and address data updated

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -786,14 +786,14 @@ export const dummyAddress = [
   {
     _id: "67b5b9e54ea97f71bbc196a0",
     userId: "67b5880e4d09769c5ca61644",
-    firstName: "Great",
-    lastName: "Stack",
-    email: "user.greatstack@gmail.com",
+    firstName: "Bix",
+    lastName: "Kaleen",
+    email: "bix@ferrix.com",
     street: "Street 123",
     city: "Main City",
     state: "New State",
     zipcode: 123456,
-    country: "IN",
+    country: "FE",
     phone: "1234567890",
   },
 ];
