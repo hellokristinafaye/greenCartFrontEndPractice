536b5e28b993677c9ce15fbcda1ad568936be7ea
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 17:33:21 -0700

    field styled

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -10,7 +10,7 @@ const inputfield = ({type, placeholder, name, handleChange, address}) => (
         name={name}
         value={address[name]}
         required
-        className=""
+        className="w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition"
     />
 )
 
