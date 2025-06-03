1cf5410cb222f6988d7f1275108b66da74f59b6b
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-03 14:33:50 -0700

    styling

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -21,7 +21,7 @@ const Orders = () => {
         <div className="md:p-10 p-4 space-y-4">
             <h2 className="text-lg font-medium">Orders List</h2>
             {orders.map((order, index) => (
-                <div key={index} className="flex flex-col md:grid md:grid-cols-[2fr_1fr_1fr_1fr] md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 text-gray-800">
+                <div key={index} className="flex flex-col md:items-center md:flex-row gap-5 justify-between p-5 max-w-4xl rounded-md border border-gray-300">
                     <div className="flex gap-5">
                         <img className="w-12 h-12 object-cover" src={assets.box_icon} alt="boxIcon" />
                         <>
