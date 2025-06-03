b711ff16af8791525ab627f54e9cd665a40dc3c8
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 21:14:03 -0700

    a tag updated to Link

--- client/src/pages/seller/SellerLayout.jsx
+++ client/src/pages/seller/SellerLayout.jsx
@@ -1,5 +1,6 @@
 // nav and sidebar in all seller dashboard pages.
 
+import { Link } from "react-router-dom";
 import { assets } from "../../assets/assets";
 import { useAppContext } from "../../context/AppContext";
 
@@ -16,10 +17,10 @@ const SellerLayout = () => {
 
     return (
         <>
-            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300">
-                <a href="/">
+            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white">
+                <Linkg href="/">
                     <img className="h-9" src="https://raw.githubusercontent.com/prebuiltui/prebuiltui/main/assets/dummyLogo/dummyLogoColored.svg" alt="dummyLogoColored" />
-                </a>
+                </Link>
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
                     <button className='border rounded-full text-sm px-4 py-1'>Logout</button>
