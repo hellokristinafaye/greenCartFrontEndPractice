f3817f215382171b394942601e28203256cb61dc
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 22:18:46 -0700

    offerprice onchange added

--- client/src/pages/seller/AddProduct.jsx
+++ client/src/pages/seller/AddProduct.jsx
@@ -77,7 +77,10 @@ const AddProduct = () => {
                     </div>
                     <div className="flex-1 flex flex-col gap-1 w-32">
                         <label className="text-base font-medium" htmlFor="offer-price">Offer Price</label>
-                        <input id="offer-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
+                      <input
+                            onChange={(e) => setOfferPrice(e.target.value)}
+                      value={offerPrice}
+                          id="offer-price" type="number" placeholder="0" className="outline-none md:py-2.5 py-2 px-3 rounded border border-gray-500/40" required />
                     </div>
                 </div>
                 <button className="px-8 py-2.5 bg-indigo-500 text-white font-medium rounded">ADD</button>
