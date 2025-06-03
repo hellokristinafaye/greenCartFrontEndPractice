61e83c6a08cda6ee3aa793a96ee5b472c7a8fb09
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 20:47:48 -0700

    more styling

--- client/src/components/seller/SellerLogin.jsx
+++ client/src/components/seller/SellerLogin.jsx
@@ -20,7 +20,7 @@ const SellerLogin = () => {
   return !isSeller && (
       <form onSubmit={onSubmitHandler} className="min-h-screen flex items-center text-sm text-gray-600">
           
-          <div className="">
+          <div className="flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200">
               <p className="text-2xl font-medium m-auto"><span className="text-primary">Seller</span> Login</p>
         </div>
 
