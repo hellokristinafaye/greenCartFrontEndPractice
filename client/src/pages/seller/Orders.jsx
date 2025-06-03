92ce82e56a13bf60b1c65b439b11c39e307f65c1
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-03 14:22:14 -0700

    icon updated

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useAppContext } from '../../context/AppContext'
-import { dummyOrders } from '../../assets/assets';
+import { assets, dummyOrders } from '../../assets/assets';
 
 const Orders = () => {
 
@@ -18,12 +18,12 @@ const Orders = () => {
 
     return (
       <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll">
-      <div className="md:p-10 p-4 space-y-4">
+        <div className="md:p-10 p-4 space-y-4">
             <h2 className="text-lg font-medium">Orders List</h2>
             {orders.map((order, index) => (
                 <div key={index} className="flex flex-col md:grid md:grid-cols-[2fr_1fr_1fr_1fr] md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 text-gray-800">
                     <div className="flex gap-5">
-                        <img className="w-12 h-12 object-cover opacity-60" src={boxIcon} alt="boxIcon" />
+                        <img className="w-12 h-12 object-cover opacity-60" src={assets.box_icon} alt="boxIcon" />
                         <>
                             {order.items.map((item, index) => (
                                 <div key={index} className="flex flex-col justify-center">
