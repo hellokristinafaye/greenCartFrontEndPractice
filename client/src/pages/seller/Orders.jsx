62d606f51ea669b865ab84baec972565768e07ab
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-03 14:32:08 -0700

    opacity on image

--- client/src/pages/seller/Orders.jsx
+++ client/src/pages/seller/Orders.jsx
@@ -23,10 +23,10 @@ const Orders = () => {
             {orders.map((order, index) => (
                 <div key={index} className="flex flex-col md:grid md:grid-cols-[2fr_1fr_1fr_1fr] md:items-center gap-5 p-5 max-w-4xl rounded-md border border-gray-300 text-gray-800">
                     <div className="flex gap-5">
-                        <img className="w-12 h-12 object-cover opacity-60" src={assets.box_icon} alt="boxIcon" />
+                        <img className="w-12 h-12 object-cover" src={assets.box_icon} alt="boxIcon" />
                         <>
                             {order.items.map((item, index) => (
-                                <div key={index} className="flex flex-col justify-center">
+                                <div key={index} className="flex flex-col ">
                                     <p className="font-medium">
                                         {item.product.name} <span className="text-primary">x {item.quantity}</span>
                                     </p>
