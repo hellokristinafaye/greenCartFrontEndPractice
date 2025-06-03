b29d4c21d129cb42a088567b75b08e3d2dd8470f
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 17:26:40 -0700

    inputfield started

--- client/src/pages/AddAddress.jsx
+++ client/src/pages/AddAddress.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import { assets } from '../assets/assets'
 
 // Input Field Component
-const inputfield = (() => (
+const inputfield = () => (
     <input type="text" className="" />
-))
+)
 
 const AddAddress = () => {
 
     const onSubmitHandler = async (e) => {
         e.preventDefault();
-        
+
     }
 
   return (
@@ -18,7 +18,11 @@ const AddAddress = () => {
           <p className="text-2xl md:text-3xl text-gray-500">Add Shipping <span className="font-semibold text-primary">Address</span></p>
           <div className="flex flex-col-reverse md:flex-row justify-between mt-10">
               <div className="flex-1 max-w-md">
-                  <form onSubmit={onSubmitHandler} className=""></form>
+                  <form onSubmit={onSubmitHandler} className="space-y-3 mt-6 text-sm">
+                      <div className="">
+                          <inputfield  />
+                      </div>
+                  </form>
               </div>
               <img src={assets.add_address_iamge} alt="Add Address" className="md:mr-16 mb-16 md:mt-0" />
           </div>
