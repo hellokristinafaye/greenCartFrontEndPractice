89cd27710922960fc233c14304b65673488a9a16
Kristina Faye <hellokristinafaye@gmail.com>
2025-06-02 18:07:17 -0700

    div styled

--- client/src/pages/MyOrders.jsx
+++ client/src/pages/MyOrders.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MyOrders = () => {
   return (
       <div>
-          <div className="">
+          <div className="flex flex-col items-end w-max mb-8">
           <p className="text-2xl font-medium uppercase">My Orders</p>
               <div className="w-16 h-0.5 bg-primary rounded-full"></div>
           </div>
